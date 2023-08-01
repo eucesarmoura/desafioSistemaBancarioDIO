609513865160c1c303900163885f7de9f9e5c739
César de Moura <84753451+eucesarmoura@users.noreply.github.com>
2023-07-31 23:29:48 -0300

    Atualização de README  e resolução do desafio.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# desafioSistemaBancarioDIO
-Resolução do desafio da formação de Python para Data Science do Ifood.
+<center> Feito pelo <a href="https://www.linkedin.com/in/eucesarmoura/" > César de Moura </a></center>
+
+<center> 31 de julho de 2023 </center>
+
+<br>
+<br>
+
+<img src="src/img/f5dba255-da18-427a-a02a-ca11a339c1cd.webp" alt="Descrição da imagem" width="140" />
+
+
+## Bootcamp Potência Tech 
+
+### Desafio: Criando um Sistema Bancário com Python 
+
+O Exercício apresentado tinha como objetivos desenvolver um programa que conseguisse efetuar três operações de um sistema bancário: saque, deposito e visualização de extrado.
+
+Para isso, desenvolvi um código na pasta <strong>resolução.py</strong> que:
+
+1. Imprime um menu que solicita uma entrada, podendo ser:
+- (d) Depósito
+- (s) Saldo
+- (e) Extrato
+- (q) Sair do sistema
+
+2. Processa a operação solicitada e volta ao menu novamente.
+
+<br>
+<br>
+
+
+***
+
+**Contact me**
+
+<a href="https://www.twitch.tv/eucesarmoura" target="_blank"><img src="https://img.shields.io/badge/Twitch-9146FF?style=for-the-badge&logo=twitch&logoColor=white" target="_blank"></a>
+<a href="https://discord.gg/eucesarmoura" target="_blank"><img src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white" target="_blank"></a> 
+<a href = "mailto:cesarhenriquedemoura@gmail.com"><img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white" target="_blank"></a>
+<a href="https:///in/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a> 
+  
+##
--- resolucao.py
+++ resolucao.py
@@ -1,12 +1,10 @@
 menu = """
-
 [d] Depositar
 [s] Sacar
 [e] Extrato
 [q] Sair
 
 => """
-
 
 saldo = 0
 limite = 500
@@ -15,7 +13,6 @@ numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
-
     opcao = input(menu)
 
     if opcao == 'd':
@@ -24,45 +21,38 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
-
         else:
             print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "s":
-
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
-
         excedeu_limite = valor > limite
-
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
             print('Operação falhou! Você não tem saldo suficiente.')
-
         elif excedeu_limite:
             print('Operação falhou! O valor do saque excede o limite.')
-
         elif excedeu_saques:
             print('Operação falhou! Número máximo de saques excedidos.')
-
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
-
         else:
             print('Operação falhou! O valor informado é inválido.')
-            
+
     elif opcao == 'e':
         print("\n========================================= EXTRATO =========================================")
         print("Não foram realizadas movimentações." if not extrato else extrato)
-        print("f\nSaldo: R$ {saldo:.2f}")
+        print(f'Saldo: R$ {saldo:.2f}')
         print("=============================================================================================")
 
     elif opcao == 'q':
+        print("Saindo do programa. Obrigado por usar nosso sistema!")
+        break
+
+    else:
         print("Operação inválida. Selecione por favor a operação desejada.")
-
-
-
